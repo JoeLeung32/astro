ac3cf9a97ec8c06aed4ea68de78ceab4d61d6b66
Joe Leung <joeleung32@yahoo.com.hk>
2023-12-08 22:57:31 +0800

    Test another way to integrate Strapi

---
+++ src/interface/place_region.ts
@@ -0,0 +1,11 @@
+export default interface Place_Region {
+	id: number;
+	attributes: {
+		name: string;
+		abbreviation: string;
+		type: string;
+		createdAt: string;
+		updatedAt: string;
+		publishedAt: string;
+	}
+}
